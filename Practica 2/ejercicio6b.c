b75543c3a282749cfba43cb6617088360f5bcc08
danist7 <danielstlmf7@gmail.com>
2018-03-23 15:37:26 +0100

    6b done

---
+++ Practica 2/ejercicio6b.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#define NUM_PROC 5
+
+
+void captura(int sennal)
+{
+ printf("\nSoy %d y he recibido la señal SIGTERM\n", getpid());
+ exit(0);
+}
+
+
+int main (void){
+  int pid, counter;
+  if (signal(SIGTERM, captura) == SIG_ERR){
+    puts("Error en la captura");
+    exit (EXIT_FAILURE);
+  }
+  pid = fork();
+  if (pid == 0){
+    while(1){
+      for (counter = 0; counter < NUM_PROC; counter++){
+        printf("%d\n", counter);
+        sleep(1);
+      }
+      sleep(3);
+    }
+  }
+  else{
+    sleep(40);
+    kill(pid,SIGTERM);
+  }
+  while(wait(NULL)>0);
+}
